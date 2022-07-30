f00375cf9c1c57a798275f4cdf3b9eb9f37e3cb6
Grek Maria <grekmariaa@gmail.com>
2022-07-30 18:48:44 +0300

    edited

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,19 +1,15 @@
-// Напиши скрипт, який:
-// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-
 const allCaregories = document.querySelectorAll("li.item")
-console.log(`Number of categories: ${allCaregories.length}`);// Number of categories: 3
+console.log(`Number of categories: ${allCaregories.length}`);
 
 const firstElOfCategories = allCaregories[0];
 const secondElOfCategories = allCaregories[1];
 const thirdElOfCategories = allCaregories[2];
 
 
-console.log(`Category: ${firstElOfCategories.firstElementChild.textContent}`);// Category: Animals
-console.log(`Elements: ${firstElOfCategories.querySelectorAll("li").length}`);// Elements: 4
-console.log(`Category: ${secondElOfCategories.firstElementChild.textContent}`);// Category: Products
-console.log(`Elements: ${secondElOfCategories.querySelectorAll("li").length}`);// Elements: 3
-console.log(`Category: ${thirdElOfCategories.firstElementChild.textContent}`);// Category: Technologies
-console.log(`Elements: ${thirdElOfCategories.querySelectorAll("li").length}`);// Elements: 5
+console.log(`Category: ${firstElOfCategories.firstElementChild.textContent}`);
+console.log(`Elements: ${firstElOfCategories.querySelectorAll("li").length}`);
+console.log(`Category: ${secondElOfCategories.firstElementChild.textContent}`);
+console.log(`Elements: ${secondElOfCategories.querySelectorAll("li").length}`);
+console.log(`Category: ${thirdElOfCategories.firstElementChild.textContent}`);
+console.log(`Elements: ${thirdElOfCategories.querySelectorAll("li").length}`);
 
