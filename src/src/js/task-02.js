01856cb44efbab1db1cbb01b333d752348fb9d39
Grek Maria <grekmariaa@gmail.com>
2022-07-30 18:43:02 +0300

    task 2 improved

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,3 +1,9 @@
+//Напиши скрипт, який для кожного елемента масиву ingredients:
+//1 Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//2 Додасть назву інгредієнта як його текстовий вміст.
+//3 Додасть елементу клас item.
+//4 Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -7,40 +13,71 @@ const ingredients = [
   "Condiments",
 ];
 
+
 const ingredientsEl = document.querySelector("#ingredients");
 
-const firstElementInIngredientsList = document.createElement("li");
-firstElementInIngredientsList.textContent = ingredients[0];
-firstElementInIngredientsList.classList.add("item");
+const elements = ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
 
-const secondElementInIngredientsList = document.createElement("li");
-secondElementInIngredientsList.textContent = ingredients[1];
-secondElementInIngredientsList.classList.add("item");
+  return element;
+});
 
-const thirdElementInIngredientsList = document.createElement("li");
-thirdElementInIngredientsList.textContent = ingredients[2];
-thirdElementInIngredientsList.classList.add("item");
+ingredientsEl.append(...elements)
 
-const fourthElementInIngredientsList = document.createElement("li");
-fourthElementInIngredientsList.textContent = ingredients[3];
-fourthElementInIngredientsList.classList.add("item");
 
-const fifthElementInIngredientsList = document.createElement("li");
-fifthElementInIngredientsList.textContent = ingredients[4];
-fifthElementInIngredientsList.classList.add("item");
+//варіант з фором
+// const ingredientsEl = document.querySelector("#ingredients");
 
-const sixthElementInIngredientsList = document.createElement("li");
-sixthElementInIngredientsList.textContent = ingredients[5];
-sixthElementInIngredientsList.classList.add("item");
+// let arreyEl = []
+// for (const ingredient of ingredients) {
+//   const element = document.createElement("li");
+//   element.textContent = ingredient;
+//   element.classList.add("item");
+//   arreyEl.push(element)
+// }
 
-ingredientsEl.append(
-  firstElementInIngredientsList,
-  secondElementInIngredientsList,
-  thirdElementInIngredientsList,
-  fourthElementInIngredientsList,
-  fifthElementInIngredientsList,
-  sixthElementInIngredientsList
-);
+// ingredientsEl.append(...arreyEl)
+
+
+
+//варіант поганий
+//const ingredientsEl = document.querySelector("#ingredients");
+// const firstElementInIngredientsList = document.createElement("li");
+// firstElementInIngredientsList.textContent = ingredients[0];
+// firstElementInIngredientsList.classList.add("item");
+
+// const secondElementInIngredientsList = document.createElement("li");
+// secondElementInIngredientsList.textContent = ingredients[1];
+// secondElementInIngredientsList.classList.add("item");
+
+// const thirdElementInIngredientsList = document.createElement("li");
+// thirdElementInIngredientsList.textContent = ingredients[2];
+// thirdElementInIngredientsList.classList.add("item");
+
+// const fourthElementInIngredientsList = document.createElement("li");
+// fourthElementInIngredientsList.textContent = ingredients[3];
+// fourthElementInIngredientsList.classList.add("item");
+
+// const fifthElementInIngredientsList = document.createElement("li");
+// fifthElementInIngredientsList.textContent = ingredients[4];
+// fifthElementInIngredientsList.classList.add("item");
+
+// const sixthElementInIngredientsList = document.createElement("li");
+// sixthElementInIngredientsList.textContent = ingredients[5];
+// sixthElementInIngredientsList.classList.add("item");
+
+// ingredientsEl.append(
+//   firstElementInIngredientsList,
+//   secondElementInIngredientsList,
+//   thirdElementInIngredientsList,
+//   fourthElementInIngredientsList,
+//   fifthElementInIngredientsList,
+//   sixthElementInIngredientsList
+// );
+
+
 
 
 
