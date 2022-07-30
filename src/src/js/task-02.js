5c75835735df6aef74f04b4a1f690f42ef6cbe3c
Grek Maria <grekmariaa@gmail.com>
2022-07-30 18:12:01 +0300

    task 2 is done

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,46 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const firstElementInIngredientsList = document.createElement("li");
+firstElementInIngredientsList.textContent = ingredients[0];
+firstElementInIngredientsList.classList.add("item");
+
+const secondElementInIngredientsList = document.createElement("li");
+secondElementInIngredientsList.textContent = ingredients[1];
+secondElementInIngredientsList.classList.add("item");
+
+const thirdElementInIngredientsList = document.createElement("li");
+thirdElementInIngredientsList.textContent = ingredients[2];
+thirdElementInIngredientsList.classList.add("item");
+
+const fourthElementInIngredientsList = document.createElement("li");
+fourthElementInIngredientsList.textContent = ingredients[3];
+fourthElementInIngredientsList.classList.add("item");
+
+const fifthElementInIngredientsList = document.createElement("li");
+fifthElementInIngredientsList.textContent = ingredients[4];
+fifthElementInIngredientsList.classList.add("item");
+
+const sixthElementInIngredientsList = document.createElement("li");
+sixthElementInIngredientsList.textContent = ingredients[5];
+sixthElementInIngredientsList.classList.add("item");
+
+ingredientsEl.append(
+  firstElementInIngredientsList,
+  secondElementInIngredientsList,
+  thirdElementInIngredientsList,
+  fourthElementInIngredientsList,
+  fifthElementInIngredientsList,
+  sixthElementInIngredientsList
+);
+
+
+
