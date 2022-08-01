4bdf559a1bc83f7063abbafa3bba022e18aba241
Grek Maria <grekmariaa@gmail.com>
2022-08-01 12:05:31 +0300

    task 1 edit

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,15 +1,7 @@
-const allCaregories = document.querySelectorAll("li.item")
+const allCaregories = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${allCaregories.length}`);
 
-const firstElOfCategories = allCaregories[0];
-const secondElOfCategories = allCaregories[1];
-const thirdElOfCategories = allCaregories[2];
-
-
-console.log(`Category: ${firstElOfCategories.firstElementChild.textContent}`);
-console.log(`Elements: ${firstElOfCategories.querySelectorAll("li").length}`);
-console.log(`Category: ${secondElOfCategories.firstElementChild.textContent}`);
-console.log(`Elements: ${secondElOfCategories.querySelectorAll("li").length}`);
-console.log(`Category: ${thirdElOfCategories.firstElementChild.textContent}`);
-console.log(`Elements: ${thirdElOfCategories.querySelectorAll("li").length}`);
-
+for (const category of allCaregories) {
+    console.log(`Category:  ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.querySelectorAll('li').length}`);
+}
