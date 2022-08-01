332f5bdcd511b6fac48853439588bd641441e296
Grek Maria <grekmariaa@gmail.com>
2022-08-01 12:27:27 +0300

    task 3 add css-properties

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -2,6 +2,6 @@ const allCaregories = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${allCaregories.length}`);
 
 for (const category of allCaregories) {
-    console.log(`Category:  ${category.firstElementChild.textContent}`);
+    console.log(`Category: ${category.firstElementChild.textContent}`);
     console.log(`Elements: ${category.querySelectorAll('li').length}`);
 }
