b8ad503a31fabba87777b1a866bb63137dd01230
Grek Maria <grekmariaa@gmail.com>
2022-08-03 14:03:44 +0300

    task 08 is done

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    if (email === '' || password === '') {
+        alert(`Усі поля форми мають бути заповнені!`);
+    } else {
+        console.log(`Email: ${email}, Password: ${password}`);
+        event.currentTarget.reset();
+    }
+}
