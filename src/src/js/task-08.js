db1f1e2db3b465ef395992913def2ec965665b36
Grek Maria <grekmariaa@gmail.com>
2022-08-04 15:05:31 +0300

    Update task-08.js

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -7,11 +7,14 @@ function onFormSubmit(event) {
 
     const email = event.currentTarget.elements.email.value;
     const password = event.currentTarget.elements.password.value;
+    let info = { email: '', password: '' };
 
     if (email === '' || password === '') {
         alert(`Усі поля форми мають бути заповнені!`);
     } else {
-        console.log(`Email: ${email}, Password: ${password}`);
+        info.email = email;
+        info.password = password;
+        console.log(info);
         event.currentTarget.reset();
     }
 }
