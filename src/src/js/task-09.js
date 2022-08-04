f1122a33169b2dd164d41758326147f85a4e9db1
Grek Maria <grekmariaa@gmail.com>
2022-08-04 11:41:17 +0300

    task 09 is done

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    colorEl.textContent = getRandomHexColor();
 }
